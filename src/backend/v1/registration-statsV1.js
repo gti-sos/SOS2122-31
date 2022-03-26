381fc26d7901377fca79ef3484bd47f41918c354
IsmaelMesaZyout <ismaelmesazyout@gmail.com>
2022-03-26 22:01:59 +0100

    final-final

--- src/backend/v1/registration-statsV1.js
+++ src/backend/v1/registration-statsV1.js
@@ -186,6 +186,7 @@ module.exports.register = (app) => {
         res.sendStatus(200, "OK.")
         
     });
+    
     app.get(BASE_API_URL + "/registration-stats/docs", (res,req) =>{
         res.redirect("https://documenter.getpostman.com/view/19505610/UVyn2Jnb")
     });
