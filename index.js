b4949c2127295cbd0381b4e312e21432378b1b24
IsmaelMesaZyout <ismaelmesazyout@gmail.com>
2022-03-26 20:43:06 +0100

    actualizado

--- index.js
+++ index.js
@@ -3,106 +3,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+var path = require('path');
+
+const BASE_API_URL = "/api/v1";
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const registration_statsV1 =  require("./src/backend/v1/registration-statsV1");
+registration_statsV1.register(app);
 
 app.use("/", express.static("public"));
-
 
 app.get("/cool", (req,res)=>{
     console.log("Requested /cool route");
     res.send("<html><body><h1>"+ cool() + "</h1></body></html>");
 });
-const BASE_API_URL = "/api/v1";
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
 
 
-
-var registration_stats = [
-    {
-        country: "germany",
-        year: 2019,
-        primarylevel: 103
-    } 
-
-];
-
-app.get(BASE_API_URL + "/registration-stats", (req,res)=>{
-    res.send(JSON.stringify(registration_stats, null,2));
-});
-
-app.post(BASE_API_URL + "/registration-stats", (req,res)=>{
-    registro = registration_stats.push(req.body);
-    if(registro == 1){
-        res.sendStatus(409, "Conflict");
-    }else{
-        res.send(JSON.stringify(registro, null,2));
-    }
-    //registro = registration_stats.push(req.body);
-    //res.sendStatus(201, "CREATED");
-});
-
-app.delete(BASE_API_URL + "/registration-stats", (req,res)=>{ 
-    registration_stats = [];
-    res.sendStatus(200, "OK");
-});
-
-app.get(BASE_API_URL + "/registration-stats/:country", (req,res)=>{
-    var countryName = req.params.country;
-    filteredCountry = registration_stats.filter((cont) =>{
-    return (cont.country == countryName); 
-    
-    });
-
-    if(filteredCountry == 0){
-        res.sendStatus(404, "NOT FOUND");
-    }else{
-        res.send(JSON.stringify(filteredCountry, null,2));
-    }
-});
-
-
-app.get(BASE_API_URL + "/registration-stats/:country/:year", (req,res)=>{
-    var countryName = req.params.country;
-    var yearName = req.params.year;
-    filteredYear = registration_stats.filter((cont) =>{
-    return (cont.country == countryName) && (cont.year == yearName); 
-    
-    });
-
-    if(filteredYear == 0){
-        res.sendStatus(404, "NOT FOUND");
-    }else{
-        res.send(JSON.stringify(filteredYear, null,2));
-    }
-});
-
-
-
-app.delete(BASE_API_URL + "/registration-stats/:country", (req,res)=>{ //borrar todos los recursos
-    var countryName = req.params.country;
-    registration_stats.filter((cont) =>{
-        return (cont.country == countryName); 
-    });
-    res.sendStatus(200, "OK");
-});
-
-app.delete(BASE_API_URL + "/registration-stats/:country/:year", (req,res)=>{ //borrar todos los recursos
-    var countryName = req.params.country;
-    var yearName = req.params.year;
-    registration_stats.filter((cont) =>{
-        return (cont.country == countryName) && (cont.year == yearName); 
-    });
-    res.sendStatus(200, "OK");
-});
-
-app.put(BASE_API_URL + "/registration-stats", (req,res)=>{
-    res.sendStatus(409, "CONFLICT");
-});
-
-app.put(BASE_API_URL + "/registration-stats/:country", (req,res)=>{
-    var countryName = req.params.country;
-});
 app.listen(port, () =>{
     console.log(`server TRULY ready at the port ${port}`);
 });
---
+++ src/backend/v1/registration-statsV1.js
@@ -0,0 +1,326 @@
+const BASE_API_URL = "/api/v1";
+var Datastore = require("nedb");
+
+var path = require('path');
+var datafile = path.join(__dirname, 'registration-stats.db');
+var db = new Datastore({ filename: datafile, autoload: true});
+
+const bodyParser = require("body-parser");
+var registration_stats = [
+    {
+        country: "germany",
+        year: 2019,
+        primarylevel: 103,
+        secondarylevel: 98,
+        tertiarylevel: 74
+
+    },
+    {
+        country: "españa",
+        year: 2015,
+        primarylevel: 106,
+        secondarylevel: 125,
+        tertiarylevel: 86
+
+    },
+
+    {
+        country: "eeuu",
+        year: 2005,
+        primarylevel: 102,
+        secondarylevel: 95,
+        tertiarylevel: 81
+
+    },
+
+    {
+        country: "canada",
+        year: 1995,
+        primarylevel: 102,
+        secondarylevel: 105,
+        tertiarylevel: 91
+
+    },
+
+    {
+        country: "indonesia",
+        year: 1992,
+        primarylevel: 111,
+        secondarylevel: 44,
+        tertiarylevel: 9
+
+    },
+    {
+        country: "china",
+        year: 1987,
+        primarylevel: 126,
+        secondarylevel: 35,
+        tertiarylevel: 3
+
+    },
+    {
+        country: "francia",
+        year: 2001,
+        primarylevel: 100,
+        secondarylevel: 105,
+        tertiarylevel: 50
+
+    },
+    {
+        country: "santaLucia",
+        year: 1974,
+        primarylevel: 114,
+        secondarylevel: 30,
+        tertiarylevel: 0 
+
+    },
+    {
+        country: "argentina",
+        year: 2010,
+        primarylevel: 117,
+        secondarylevel: 100,
+        tertiarylevel: 73 
+
+    }
+
+];
+    
+module.exports.register = (app) => {
+    
+    app.get(BASE_API_URL + "/registration-stats/docs", (res,req) =>{
+        res.redirect("http://sos2122-31.herokuapp.com/api/v1/registration-stats/docs")
+    });
+
+
+    //app.get(BASE_API_URL + "/registration-stats", (req,res)=>{
+    //    res.send(JSON.stringify(registration_stats, null,2));
+    //});
+
+    //app.get(BASE_API_URL + "/registration-stats/loadInitialData", (req,res)=>{
+    //    db.insert(registration_stats);
+    //    res.send(JSON.stringify(registration_stats, null,2));
+    //});
+
+    app.get(BASE_API_URL + "/registration-stats", (req,res)=>{
+        db.insert(registration_stats);
+        res.send(JSON.stringify(registration_stats, null,2));
+    });
+
+    app.get(BASE_API_URL + "/registration-stats/loadInitialData",(req, res)=>{
+    
+        if(registration_stats.length==0){
+            registration_stats = [
+                {
+                    country: "germany",
+                    year: 2019,
+                    primarylevel: 103,
+                    secondarylevel: 98,
+                    tertiarylevel: 74
+            
+                },
+                {
+                    country: "españa",
+                    year: 2015,
+                    primarylevel: 106,
+                    secondarylevel: 125,
+                    tertiarylevel: 86
+            
+                },
+            
+                {
+                    country: "eeuu",
+                    year: 2005,
+                    primarylevel: 102,
+                    secondarylevel: 95,
+                    tertiarylevel: 81
+            
+                },
+            
+                {
+                    country: "canada",
+                    year: 1995,
+                    primarylevel: 102,
+                    secondarylevel: 105,
+                    tertiarylevel: 91
+            
+                },
+            
+                {
+                    country: "indonesia",
+                    year: 1992,
+                    primarylevel: 111,
+                    secondarylevel: 44,
+                    tertiarylevel: 9
+            
+                },
+                {
+                    country: "china",
+                    year: 1987,
+                    primarylevel: 126,
+                    secondarylevel: 35,
+                    tertiarylevel: 3
+            
+                },
+                {
+                    country: "francia",
+                    year: 2001,
+                    primarylevel: 100,
+                    secondarylevel: 105,
+                    tertiarylevel: 50
+            
+                },
+                {
+                    country: "santaLucia",
+                    year: 1974,
+                    primarylevel: 114,
+                    secondarylevel: 30,
+                    tertiarylevel: 0 
+            
+                },
+                {
+                    country: "argentina",
+                    year: 2010,
+                    primarylevel: 117,
+                    secondarylevel: 100,
+                    tertiarylevel: 73 
+            
+                }
+            
+            ];
+        }
+        res.sendStatus(200, "OK.")
+        
+    });
+
+    app.get(BASE_API_URL + "/registration-stats/:country", (req,res)=>{
+        var countryName = req.params.country;
+        filteredCountry = registration_stats.filter((cont) =>{
+        return (cont.country == countryName); 
+        
+        });
+    
+        if(filteredCountry == 0){
+            res.sendStatus(404, "NOT FOUND");
+        }else{
+            res.send(JSON.stringify(filteredCountry, null,2));
+        }
+    });
+    
+    
+    app.get(BASE_API_URL + "/registration-stats/:country/:year", (req,res)=>{
+        var countryName = req.params.country;
+        var yearName = req.params.year;
+        filteredYear = registration_stats.filter((cont) =>{
+        return (cont.country == countryName) && (cont.year == yearName); 
+        
+        });
+    
+        if(filteredYear == 0){
+            res.sendStatus(404, "NOT FOUND");
+        }else{
+            res.send(JSON.stringify(filteredYear, null,2));
+        }
+    });
+
+
+    //app.get(BASE_API_URL + "/registration-stats", (req,res)=>{
+    //    res.send(JSON.stringify(registration_stats, null,2));
+    //})
+
+
+
+    app.post(BASE_API_URL + "/registration-stats/:country", (req,res)=>{
+        res.sendStatus(405, "Method not allowed");
+    });
+    app.post(BASE_API_URL + "/registration-stats/:year", (req,res)=>{
+        res.sendStatus(405, "Method not allowed");
+    });
+
+    function parametroscorrectos(req){
+        return (req.body.country == null |
+                 req.body.year == null | 
+                 req.body.primarylevel == null | 
+                 req.body.secondarylevel == null | 
+                 req.body.tertiarylevel == null);
+    };
+
+    app.post(BASE_API_URL+ "/registration-stats",(req, res)=>{
+        if(parametroscorrectos(req)){
+            res.sendStatus(400,"BAD REQUEST - Parametros incorrectos");
+        }else{ 
+            var postRegistration = registration_stats.filter((reg)=>{
+            return (req.body.country == reg.country && req.body.year == reg.year)
+        });
+        if(postRegistration.length != 0){
+            res.sendStatus(409,"CONFLICT");
+        }else{
+            registration_stats.push(req.body);
+            res.sendStatus(201,"CREATED");
+        }
+    }
+    });
+
+    app.delete(BASE_API_URL + "/registration-stats", (req,res)=>{ 
+        registration_stats = [];
+        res.sendStatus(200, "OK");
+    });
+    
+    
+    app.delete(BASE_API_URL + "/registration-stats/:country", (req,res)=>{ //borrar todos los recursos
+        var countryName = req.params.country;
+        registration_stats.filter((cont) =>{
+            return (cont.country == countryName); 
+        });
+        res.sendStatus(200, "OK");
+    });
+    
+    app.delete(BASE_API_URL + "/registration-stats/:country/:year", (req,res)=>{ //borrar todos los recursos
+        var countryName = req.params.country;
+        var yearName = req.params.year;
+        registration_stats.filter((cont) =>{
+            return (cont.country == countryName) && (cont.year == yearName); 
+        });
+        res.sendStatus(200, "OK");
+    });
+    
+    
+
+
+
+    
+
+    app.put(BASE_API_URL + "/registration-stats", (req,res)=>{
+        res.sendStatus(405, "Method Not Allowed");
+    });
+
+
+    app.put(BASE_API_URL+"/registration-stats/:country/:year",(req, res)=>{
+        
+        if(parametroscorrectos(req)){
+            res.sendStatus(400,"BAD REQUEST - Parametros incorrectos");
+        }else{
+            var country = req.params.country;
+            var year = req.params.year;
+            var body = req.body;  
+            var tl = registration_stats.findIndex((reg) =>{
+                return (reg.country == country && reg.year == year)
+            });
+            if(tl == null){
+                res.sendStatus(404,"NOT FOUND");
+            }else if(country != body.country || year != body.year){
+                res.sendStatus(400,"BAD REQUEST");
+            }else{
+                var  update_registrations = {...body};
+                registration_stats[tl] = update_registrations;
+            
+                res.sendStatus(200,"UPDATED");
+            }
+        }
+    
+    });
+    
+
+
+
+};
+
