c9255844bc8d311ba22af54bfb4a9480c800a9ba
Ismael <ismaelmesazyout@gmail.com>
2022-04-03 16:33:10 +0200

    ola

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const port = process.env.PORT || 8080;
 
 const registration_stats_V1 = require("./src/backend/v1/registration-statsV1");
 const Datastore = require('nedb');
-//db_regitration_stats = new Datastore();
+db_regitration_stats = new Datastore();
 
 app.use(bodyParser.json());
 
-registration_stats_V1.register(app);
+registration_stats_V1.register(app,db_regitration_stats);
 
-
-//José Ortiz Roldán
-const proportion_statsV1 = require("./src/backend/v1/proportion-statsV1");
-proportion_statsV1.register(app);
 
 //José Ortiz Roldán
 const proportion_statsV1 = require("./src/backend/v1/proportion-statsV1");
--- src/backend/v1/registration-statsV1.js
+++ src/backend/v1/registration-statsV1.js
@@ -1,10 +1,5 @@
 const BASE_API_URL = "/api/v1";
 const bodyParser = require("body-parser");
-var Datastore = require("nedb");
-
-var path = require('path');
-var datafile = path.join(__dirname, 'registration-stats.db');
-var db = new Datastore({filename: datafile, autoload:true});
 
 var registration_stats = [{
     country: "germany",
@@ -85,7 +80,7 @@ var registration_stats = [{
 ];
 
 
-module.exports.register = (app) => {
+module.exports.register = (app,db) => {
 
     app.get(BASE_API_URL + "/registration-stats/loadInitialData", (req, res) => {
         db.insert(registration_stats);
