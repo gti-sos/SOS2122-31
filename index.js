3ed318f900270f45c8833b7eb29bd1f71f148363
madgIitch <peorrr9@gmail.com>
2022-04-04 16:52:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ registration_stats_V1.register(app,db_regitration_stats);
 
 //José Ortiz Roldán
 const proportion_statsV1 = require("./src/backend/v1/proportion-statsV1");
-proportion_statsV1.register(app);
+var datafile2 = path.join(__dirname, 'proportion-stats.db');
+
+db_proportion_stats = new Datastore({filename: datafile2, autoload:true});
+proportion_statsV1.register(app, db_proportion_stats);  
+
 
 //Daniel Vega Vera:
 const alphabetization_stats_V1 = require("./src/backend/v1/alphabetization-statsV1");
