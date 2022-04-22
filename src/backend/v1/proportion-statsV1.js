a984a63a90e8a000ed01398f2796e4b892a02383
madgIitch <peorrr9@gmail.com>
2022-04-22 11:58:26 +0200

    Update proportion-statsV1.js

--- src/backend/v1/proportion-statsV1.js
+++ src/backend/v1/proportion-statsV1.js
@@ -386,7 +386,6 @@ module.exports.register = (app,db) => {
         var yearRegis = req.params.year;
         var body = req.body;  
 
-        
         db.find({},function(err,regisNew){
             if(err){
                 res.sendStatus(500, "ERROR EN CLIENTE");
