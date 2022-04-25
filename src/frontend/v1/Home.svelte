b3a37faf21c0c1a3af561747880b6a23feed58a2
Ismael <ismaelmesazyout@gmail.com>
2022-04-25 11:16:42 +0200

    updated Ismael

--- src/frontend/v1/Home.svelte
+++ src/frontend/v1/Home.svelte
@@ -7,11 +7,11 @@
         - [Daniel Vega Vera](https://github.com/daniveg20)
         - [José Ortiz Roldán](https://github.com/madgIitch)
         <h3>Project description:</h3> Nuestras fuentes de información esta orientadas a analizar la relación que existe entre los matriculados el los diferentes niveles escolares, la tasa de alfabetización y proporción sin educación, empleo ni capacitación en jóvenes
-        <h3>Repository:</h3>[gti-sos/SOS2122-31](https://github.com/gti-sos/SOS2122-31)
-        <h3>URL:</h3>[http://sos2122-31.herokuapp.com](http://sos2122-31.herokuapp.com)
-        <p style="font-weight: bold;">
-            -  APIs:
+        <h3>Repository:</h3><a href="https://github.com/gti-sos/SOS2122-31">[gti-sos/SOS2122-31](https://github.com/gti-sos/SOS2122-31)</a>
+        <h3>URL:</h3><a href="https://sos2122-31.herokuapp.com/">[http://sos2122-31.herokuapp.com](http://sos2122-31.herokuapp.com)</a>
+        <p style="font-weight: bold;">            -  APIs:
         </p>
+        
         <ul>
             <li><a href="https://sos2122-31.herokuapp.com/api/v1/registration-stats/docs">[https://sos2122-31.herokuapp.com/api/v1/registration-stats/docs]</a>(developed by [Ismael Mesa Zyout] <a href="https://github.com/IsmaelMesaZyout">
             </a>)</li>
@@ -21,7 +21,18 @@
             </a>)</li>
         </ul>
         
-    
+     <p style="font-weight: bold;">
+            -  APIs:
+        </p>
+        
+        <ul>
+            <li><a href="https://sos2122-31.herokuapp.com/api/v1/registration-stats/docs">[https://sos2122-31.herokuapp.com/api/v1/registration-stats/docs]</a>(developed by [Ismael Mesa Zyout] <a href="https://github.com/IsmaelMesaZyout">
+            </a>)</li>
+            <li><a href="https://sos2122-31.herokuapp.com/api/v1/alphabetization-stats/docs">[https://sos2122-31.herokuapp.com/api/v1/alphabetization-stats/docs]</a>(developed by [Daniel Vega Vera] <a href="https://github.com/daniveg20">
+            </a>)</li>
+            <li><a href="https://sos2122-31.herokuapp.com/api/v1/proportion-stats/docs">[https://sos2122-31.herokuapp.com/api/v1/proportion-stats/docs]</a>(developed by [José Ortiz Roldán] <a href="https://github.com/madgIitch">
+            </a>)</li>
+        </ul>
         <h6>
             Ismael Mesa Zyout:
             <a href="https://sos2122-31.herokuapp.com/api/v1/registration-stats">Enlace BackEnd</a>
