0a2dac3bd83bbf966dd813c702e5bfeb5d0e6864
daniveg20 <danielvegavera@gmail.com>
2022-04-25 15:28:04 +0200

    Cambios finales

--- src/frontend/v1/alphabetization-stats/AlphabetizationStats.svelte
+++ src/frontend/v1/alphabetization-stats/AlphabetizationStats.svelte
@@ -1,320 +1,269 @@
 <script>
-	import { onMount } from "svelte";
-	import Alert from "sveltestrap/src/Alert.svelte";
-	import Table from "sveltestrap/src/Table.svelte";
-	import Button from "sveltestrap/src/Button.svelte";
-	import { Pagination, PaginationItem, PaginationLink } from "sveltestrap";
+
+    import { onMount } from 'svelte';
+	import Table from 'sveltestrap/src/Table.svelte';
+	import Button from 'sveltestrap/src/Button.svelte';
+	import Alert from 'sveltestrap/src/Alert.svelte';
+
+
+    let registrations = [];
+
+	let from = null;
+	let to = null;
+	let offset = 0;
+	let limit = 10;
+	const BASE_API_URL = "/api/v1";
+
+	let paginasMax = 0;
+	let numEntries;
 
 	let errorMsg = "";
 	let okMsg = "";
-	let registrations = [];
 	let newRegistration = {
 		country: "",
 		year: "",
 		ar_ym: "",
 		ar_yw: "",
 		ar_ty: "",
+		
 	};
 	let visible = false;
 	let visibleOk = false;
 
-	let offset1 = 0;
-	let offset2 = 0;
-	let limit = 10;
-	let page = 1;
-	let lastPage = 1;
-	let total = 0;
-	let from = null;
-	let to = null;
-	onMount(getReg1);
+    onMount(getReg);
 
-	const BASE_API_URL = "/api/v1";
-
-	async function getReg() {
-		console.log("Fetching entries....");
-		let cadena = `/api/v1/alphabetization-stats?from=${from}&&to=${to}&&`;
-		const res = await fetch(cadena);
-		if (res.ok) {
-			const data = await res.json();
-			registrations = data;
-			console.log("Received entries: " + registrations.length);
-		} else {
-			console.log("Error");
+    async function getReg(){
+        console.log("Fetching registrations....");
+		let c = `/api/v1/alphabetization-stats?limit=${limit}&&offset=${offset*10}&&`;
+		if (from != null) {
+			c = c + `from=${from}&&`
 		}
-	}
-
-	async function getReg1() {
-		console.log("Fetching entries....");
-		let cadena =
-			"/api/v1/alphabetization-stats?limit=" + limit + "&offset=" + offset1;
-		const res = await fetch(cadena);
-		if (res.ok) {
-			//let cadenaPag = cadena.split("?limit=" + limit + "&offset=" + offset1);
-			paginacion();
-			const data = await res.json();
-			registrations = data;
-			console.log("Received entries: " + registrations.length);
+		if (to != null) {
+			c = c + `to=${to}&&`
 		}
-	}
-
-	async function paginacion() {
-		const data = await fetch(BASE_API_URL + "/alphabetization-stats");
-		if (data.status == 200) {
-			const json = await data.json();
-			total = json.length;
-			cambiapag(page, offset1);
+        const res = await fetch(c); 
+        if(res.ok){
+			let cadenaPag = c.split(`limit=${limit}&&offset=${offset*10}`);
+			maxPagesFunction(cadenaPag[0]+cadenaPag[1]);
+            const data = await res.json();
+            registrations = data;
+			numEntries = registrations.length;
+            console.log("Received entries: "+registrations.length);
+        }else{
+			Errores(res.status);
 		}
-	}
-
-	function range(size, start = 0) {
-		return [...Array(size).keys()].map((i) => i + start);
-	}
-
-	function cambiapag(page1, offset2) {
-		lastPage = Math.ceil(total / 10);
-		console.log("Last page = " + lastPage);
-		if (page1 !== page) {
-			offset1 = offset2;
-			page = page1;
-			getReg1();
-			getReg();
-		}
-	}
+    }
 
 	async function insertRegistration() {
-		console.log("Inserting data " + JSON.stringify(newRegistration));
-
-		const res = await fetch(BASE_API_URL + "/alphabetization-stats", {
-			method: "POST",
-			body: JSON.stringify(newRegistration),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		}).then((res) => {
-			if (res.ok) {
-				getReg1();
+    	console.log("Inserting data "+ JSON.stringify(newRegistration));
+   		
+		const res = await fetch(BASE_API_URL +"/alphabetization-stats",
+							{
+								method: "POST",
+								body: JSON.stringify(newRegistration),
+								headers:{
+									"Content-Type": "application/json"
+								}
+							}
+		).then((res) => {
+			
+			if(res.ok){
 				getReg();
 				okMsg = "El dato se introdujo correctamente";
-				visibleOk = true;
-				visible = false;
-			} else if (res.status === 409) {
-				errorMsg = "Ya existe ese dato";
-				visibleOk = false;
-				visible = true;
-			} else if (res.status === 400) {
+				visibleOk=true;
+				visible=false;
+			}else if(res.status === 409){
+                errorMsg = "Ya existe ese dato";
+				visibleOk=false;
+				visible=true;
+			}else if(res.status === 400){
 				errorMsg = "Campo mal introducido";
-				visibleOk = false;
-				visible = true;
+				visibleOk=false;
+				visible=true;
 			}
+            
 		});
-	}
+	}	
 
-	async function BorrarRegis(country, year) {
-		console.log(`Deleting data with name ${country} and date ${year}`);
-
-		const res = await fetch(
-			BASE_API_URL + "/alphabetization-stats/" + country + "/" + year,
+	async function BorrarRegis(countryDelete, yearDelete){
+        const res = await fetch("/api/v1/alphabetization-stats/"+countryDelete+"/"+yearDelete,
 			{
-				method: "DELETE",
-			}
-		).then(function (res) {
-			getReg1();
-			getReg();
-			okMsg = "Dato eliminado";
-			visibleOk = true;
-			visible = false;
-		});
-	}
-
-	async function BorrarRegistros() {
-		console.log("Deleting all data");
-
-		const res = await fetch(BASE_API_URL + "/alphabetization-stats", {
-			method: "DELETE",
-		}).then(function (res) {
-			if (res.ok) {
-				getReg1();
+				method: "DELETE"
+			}).then(function(res){
+				if(res.ok){
+					if(numEntries==1){
+					from = null;
+					to = null;
+				}
 				getReg();
-				okMsg = "Todos los datos se han eliminado";
-				visibleOk = true;
-				visible = false;
-			} else {
-				errorMsg = "No hay datos que borrar";
-				visibleOk = false;
-				visible = true;
-			}
-		});
+				okMsg = "Dato eliminado";
+				visibleOk=true;
+				visible=false;
+				}
+
+			});
+    }
+
+	async function BorrarRegistros(){
+        console.log("Deleting registrations....");
+        const res = await fetch("/api/v1/alphabetization-stats/",
+			{
+				method: "DELETE"
+			}).then(function (res){
+				from = null;
+				to = null;
+				if(res.ok){
+								getReg();
+								okMsg = "Todos los datos se han eliminado";
+								visibleOk=true;
+								visible=false;
+							}else{
+								errorMsg = "No hay datos que borrar";
+								visibleOk=false;
+								visible=true;
+							}
+							})
+		
+    }
+
+	async function CargarRegistrations(){
+        console.log("Loading registrations....");
+        const res = await fetch("/api/v1/alphabetization-stats/loadInitialData",
+			{
+				method: "GET"
+			}).then(function (res){
+				getReg();
+				window.alert("TODO CARGADO");
+			});
+    }
+
+
+	async function maxPagesFunction(cadena){
+		let num;
+        const res = await fetch(cadena,
+			{
+				method: "GET"
+			});
+			if(res.ok){
+				const data = await res.json();
+				paginasMax = Math.floor(data.length/10);
+				if(paginasMax === data.length/10){
+					paginasMax = paginasMax-1;
+				}
+        }
 	}
 
-	async function CargarRegistrations() {
-		console.log("Loading entries....");
-		const res = await fetch("/api/v1/alphabetization-stats/loadInitialData", {
-			method: "GET",
-		}).then(function (res) {
-			getReg1();
-			getReg();
-		});
-	}
 </script>
 
-<main>
-	<Alert color="danger" isOpen={visible} toggle={() => (visible = false)}>
-		{#if errorMsg}
-			<p>ERROR: {errorMsg}</p>
-		{/if}
-	</Alert>
-	<Alert
-		color="success"
-		isOpen={visibleOk}
-		toggle={() => (visibleOk = false)}
-	>
-		{#if okMsg}
-			<p>Correcto: {okMsg}</p>
-		{/if}
-	</Alert>
-	<Table bordered>
-		<thead>
-			<tr>
-				<th>Fecha inicio</th>
-				<th>Fecha fin</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="number" min="1970" bind:value={from} /></td>
-				<td><input type="number" min="1970" bind:value={to} /></td>
-				<td align="center"
-					><Button
-						outline
-						color="dark"
-						on:click={() => {
+
+<main>	
+			<Alert color="danger" isOpen={visible} toggle={() => (visible = false)}>
+				{#if errorMsg}
+					<p>ERROR: {errorMsg}</p>
+				   {/if}
+			</Alert>
+			<Alert color="success" isOpen={visibleOk} toggle={() => (visibleOk = false)}>
+				{#if okMsg}
+					<p>Correcto: {okMsg}</p>
+				{/if}
+			</Alert>
+			<Table bordered>
+				<thead>
+					<tr>
+						<th>Fecha inicio</th>
+						<th>Fecha fin</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input type="number" min="1999" bind:value="{from}"></td>
+						<td><input type="number" min="1999" bind:value="{to}"></td>
+						<td align="center"><Button outline color="dark" on:click="{()=>{
 							if (from == null || to == null) {
-								window.alert(
-									"Los campos fecha inicio y fecha fin no pueden estar vacíos"
-								);
-							} else {
+								window.alert('Los campos fecha inicio y fecha fin no pueden estar vacíos')
+							}else{
 								getReg();
 							}
-						}}
-					>
-						Buscar
-					</Button>
-				</td>
-				<td align="center"
-					><Button
-						outline
-						color="primary"
-						on:click={() => {
+						}}">
+							Buscar
+							</Button>
+						</td>
+						<td align="center"><Button outline color="primary" on:click="{()=>{
 							from = null;
 							to = null;
 							getReg();
-						}}
-					>
-						Limpiar Búsqueda
-					</Button>
-				</td>
-			</tr>
-		</tbody>
-	</Table>
-	<Table bordered>
-		<thead>
-			<tr>
-				<td
-					><Button id="charge" on:click={CargarRegistrations}
-						>Cargar datos</Button
-					></td
-				>
-				<td
-					><Button id="delete" on:click={BorrarRegistros}
-						>Borrar datos</Button
-					></td
-				>
-			</tr>
-
-			<tr>
-				<th>Ciudad</th>
-				<th>Año</th>
-				<th>Hombre</th>
-				<th>Mujer</th>
-				<th>Total</th>
-				<th>Acciones</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input bind:value={newRegistration.country} /></td>
-				<td><input bind:value={newRegistration.year} /></td>
-				<td><input bind:value={newRegistration.ar_ym} /></td>
-				<td><input bind:value={newRegistration.ar_yw} /></td>
-				<td><input bind:value={newRegistration.ar_ty} /></td>
-				<td
-					><Button
-						outline
-						color="primary"
-						on:click={insertRegistration}>Añadir</Button
-					></td
-				>
-			</tr>
-			{#each registrations as registration}
-				<tr>
-					<td>{registration.country}</td>
-					<td>{registration.year}</td>
-					<td>{registration.ar_ym}</td>
-					<td>{registration.ar_yw}</td>
-					<td>{registration.ar_ty}</td>
-					<td
-						><Button
-							outline
-							color="warning"
-							on:click={function () {
-								window.location.href = `/#/alphabetization-stats/${registration.country}/${registration.year}`;
-							}}
-						>
+							
+						}}">
+							Limpiar Búsqueda
+							</Button>
+						</td>
+					</tr>
+				</tbody>
+			</Table>			
+			<Table bordered>
+			
+				<thead>
+					<tr>
+						<td><Button id='charge' on:click={CargarRegistrations}>Cargar datos</Button></td>
+						<td><Button id='delete' on:click={BorrarRegistros}>Borrar datos</Button></td>
+							
+					</tr>
+					
+					<tr>
+						<th>Ciudad</th>
+						<th>Año</th>
+						<th>Hombre</th>
+						<th>Mujer</th>
+						<th>Total</th>
+						<th>Acciones</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><input bind:value="{newRegistration.country}"></td>
+						<td><input type="number" bind:value="{newRegistration.year}"></td>
+						<td><input type="number" bind:value="{newRegistration.ar_ym}"></td>
+						<td><input type="number" bind:value="{newRegistration.ar_yw}"></td>
+						<td><input type="number" bind:value="{newRegistration.ar_ty}"></td>
+						<td><Button 
+							outline 
+							color="primary"
+							on:click="{insertRegistration}">Añadir</Button></td>
+					</tr>
+					{#each registrations as registration}
+					<tr>
+						<td>{registration.country}</td>
+						<td>{registration.year}</td>
+						<td>{registration.ar_ym}</td>
+						<td>{registration.ar_yw}</td>
+						<td>{registration.ar_ty}</td>
+						<td><Button outline color="warning" on:click={function (){
+							window.location.href = `/#/alphabetization-stats/${registration.country}/${registration.year}`
+						}}>
 							Editar
 						</Button>
-					</td><td
-						><Button
-							outline
-							color="danger"
-							on:click={BorrarRegis(
-								registration.country,
-								registration.year
-							)}
-						>
+						<td><Button outline color="danger" on:click={BorrarRegis(registration.country,registration.year)}>
 							Borrar
 						</Button>
-					</td>
-				</tr>
-			{/each}
-		</tbody>
+						</td>
+					</tr>
+					{/each}
+					
+				</tbody>
 		<div>
-			<Pagination ariaLabel="Web pagination">
-				<PaginationItem class={page === 1 ? "enable" : ""}>
-					<PaginationLink
-						previous
-						href="#/alphabetization-stats"
-						on:click={() => cambiapag(page - 1, offset1 - 10)}
-					/>
-				</PaginationItem>
-				{#each range(lastPage, 1) as page}
-					<PaginationItem class={page === page ? "active" : ""}>
-						<PaginationLink
-							previous
-							href="#/alphabetization-stats"
-							on:click={() => cambiapag(page, (page - 1) * 10)}
-						>
-							{page}
-						</PaginationLink>
-					</PaginationItem>
-				{/each}
-				<PaginationItem class={page === lastPage ? "disabled" : ""}>
-					<PaginationLink
-						next
-						href="#/alphabetization-stats"
-						on:click={() => cambiapag(page + 1, offset1 + 10)}
-					/>
-				</PaginationItem>
-			</Pagination>
+    		
+  
+	  </div>
+	  <div align="left">
+		{#each Array(paginasMax+1) as _,page}
+		
+			<Button outline color="secondary" on:click={()=>{
+				offset = page;
+				getReg();
+			}}>{page} </Button>&nbsp
+	
+		{/each}
 		</div>
+		
 	</Table>
 </main>
