62cf7fc6eb3a5686bd78c83a23b53596d5071d68
Ismael <ismaelmesazyout@gmail.com>
2022-05-16 19:42:17 +0200

    efforts medio-terminados e integraciones terminadas ismael mesa

--- src/frontend/v1/registration-stats/graphPublic.svelte
+++ src/frontend/v1/registration-stats/graphPublic.svelte
@@ -12,6 +12,8 @@
     let nivelSecundario = ["nivelSecundario"];
     let nivelTerciario = ["nivelTerciario"];
 
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
     async function loadGraph2() {
         var chart = bb.generate({
             data: { 
@@ -58,6 +60,7 @@
         }, 0);
     }
     async function loadGraph() {
+        await delay(2000)
         const res = await fetch("/remoteAPI");
         const res1 = await fetch("/api/v2/registration-stats");
         if (res.ok) {
