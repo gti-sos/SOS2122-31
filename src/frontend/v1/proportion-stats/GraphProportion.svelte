13d72c5c9d34bb5b816cc8e95038aad2847d2b17
madgIitch <peorrr9@gmail.com>
2022-05-24 11:48:21 +0200

    actualizacion home svelte

--- src/frontend/v1/proportion-stats/GraphProportion.svelte
+++ src/frontend/v1/proportion-stats/GraphProportion.svelte
@@ -189,7 +189,7 @@
 <svelte:head>
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/highcharts-more.js"></script>
-    <script src="https://code.highcharts.com/modules/packedbubble.js"></script>
+    
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/export-data.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js"on:load="{loadGraph}"></script>
