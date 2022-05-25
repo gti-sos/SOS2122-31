b9abdeff2e9114342eaee5c7e1860a44391dd882
daniveg20 <danielvegavera@gmail.com>
2022-05-25 21:33:42 +0200

    Error tick

--- src/frontend/v1/alphabetization-stats/GraphAPISOS2_DVV.svelte
+++ src/frontend/v1/alphabetization-stats/GraphAPISOS2_DVV.svelte
@@ -1,141 +1,140 @@
 <script>
-    import { onMount } from "svelte";
-    import JSC from "jscharting";
-
-    //Atributos API Propia:
-    let apiData = [];
-    let country = [];
-    let year = [];
-    let ar_ym = [];
-    let ar_yw = [];
-    let ar_ty = [];
-
-    //Atributos API Externa:
-    let apiEXT = [];
-    let cSOS = [];
-    let ySOS = [];
-    let azfSOS = [];
-    let afsSOS = [];
-    let asSOS = [];
-
-    //API EXTERNA:
-    async function loadAPISOS1() {
-        const resEXT = await fetch("/remoteApiCancer");
-        if (resEXT.ok) {
-            const arrayEXT = await resEXT.json();
-            apiEXT = arrayEXT;
-            //Ordenamos valores:
-            console.log(apiEXT.length);
-            apiEXT.forEach((element) => {
-                cSOS.push(element.country);
-                ySOS.push(element.year);
-                azfSOS.push(element.ages_zero_fifty);
-                afsSOS.push(element.ages_fifty_seventy);
-                asSOS.push(element.ages_seventy);
+    import Button from "sveltestrap/src/Button.svelte";
+    import { pop } from "svelte-spa-router";
+    let datos = [];
+    let datos1 = [];
+    let fechas = [];
+    let fechas1 = [];
+    let kg = [];
+    let tot = [];
+    let tpc = [];
+    let nivelPrimario = [];
+    let nivelSecundario = [];
+    let nivelTerciario = [];
+    let paisss = [];
+    let paisss1 = [];
+    async function loadGraph() {
+        const res = await fetch(
+            "/remoteApiCancer"
+        );
+        const res1 = await fetch("/api/v2/alphabetization-stats");
+        if (res.ok && res1.ok) {
+            datos = await res.json();
+            datos1 = await res1.json();
+            const comun = [];
+            for (let i = 0; i < datos1.length; i++) {
+                comun.push(datos1[i].country + "/" + datos1[i].year);
+            }
+            datos1.forEach((data) => {
+                let fecha1 = data["country"] + "-" + data.year;
+                fechas.push(fecha1);
+                if (comun.includes(fecha1)) {
+                    let index = comun.indexOf(fecha1);
+                    tot.push(datos[index].ages_zero_fifty);
+                    kg.push(datos[index].ages_fifty_seventy);
+                    tpc.push(datos[index].ages_seventy);
+                    datos.splice(index, 1);
+                } else {
+                    tot.push("");
+                    kg.push("");
+                    tpc.push("");
+                }
+                nivelPrimario.push(data.ar_ym);
+                nivelSecundario.push(data.ar_yw);
+                nivelTerciario.push(data.ar_ty);
             });
-            //await delay(1000);
-            loadGraph();
+            datos.forEach((data) => {
+                fechas.push(data["country"] + "-" + data.year);
+                tot.push(data.ages_zero_fifty);
+                kg.push(data.ages_fifty_seventy);
+                tpc.push(data.ages_seventy);
+                nivelPrimario.push("");
+                nivelSecundario.push("");
+                nivelTerciario.push("");
+            });
         } else {
             window.alert("No hay datos para este pais");
             console.log("INTERNAL FATAL ERROR");
-            //window.location.href = `/#/alphabetization-stats`;
+            window.location.href = `/#/registration-stats`;
         }
-    }
-
-    //API PROPIA:
-    async function getData() {
-        const res1 = await fetch("api/v2/alphabetization-stats/");
-        if (res1.ok) {
-            const arrayData = await res1.json();
-            apiData = arrayData;
-            //Ordenamos valores:
-            apiData.sort(function (a, b) {
-                var keyA = new Date(a.year),
-                    keyB = new Date(b.year);
-                // Compare the 2 dates
-                if (keyA < keyB) return -1;
-                if (keyA > keyB) return 1;
-                return 0;
-            });
-            console.log(apiData.length);
-            apiData.forEach((element) => {
-                country.push(element.country);
-                year.push(element.year);
-                ar_ym.push(element.ar_ym);
-                ar_yw.push(element.ar_yw);
-                ar_ty.push(element.ar_ty);
-            });
-            //await delay(1000);
-            loadAPISOS1();
-            loadGraph();
-        } else {
-            window.alert("ERROR AL CONECTAR CON LA BASE DE DATOS");
-            console.log("INTERNAL FATAL ERROR");
-            window.location.href = `/#/alphabetization-stats`;
-        }
-    }
-
-    async function loadGraph() {
-        JSC.chart("chartDiv", {
-            debug: true,
-            type: "line",
-            legend_visible: false,
-            xAxis: {
-                crosshair_enabled: true,
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myChart = new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: fechas,
+                datasets: [
+                    {
+                        label: "Nivel Primario",
+                        data: nivelPrimario,
+                        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+                        borderColor: ["rgba(255, 99, 132, 1)"],
+                        borderWidth: 1,
+                    },
+                    {
+                        label: "Nivel Secundario",
+                        data: nivelSecundario,
+                        backgroundColor: ["rgba(54, 162, 235, 0.2)"],
+                        borderColor: ["rgba(54, 162, 235, 1)"],
+                        borderWidth: 1,
+                    },
+                    {
+                        label: "Nivel Terciario",
+                        data: nivelTerciario,
+                        backgroundColor: ["rgba(255, 206, 86, 0.2)"],
+                        borderColor: ["rgba(255, 206, 86, 1)"],
+                        borderWidth: 1,
+                    },
+                    {
+                        label: "tot",
+                        data: tot,
+                        backgroundColor: ["rgba(75, 192, 192, 0.2)"],
+                        borderColor: ["rgba(75, 192, 192, 1)"],
+                        borderWidth: 1,
+                    },
+                    {
+                        label: "kg",
+                        data: kg,
+                        backgroundColor: ["rgba(153, 102, 255, 0.2)"],
+                        borderColor: ["rgba(153, 102, 255, 1)"],
+                        borderWidth: 1,
+                    },
+                    {
+                        label: "tpc",
+                        data: tpc,
+                        backgroundColor: ["rgba(255, 159, 64, 0.2)"],
+                        borderColor: ["rgba(255, 159, 64, 1)"],
+                        borderWidth: 1,
+                    },
+                ],
             },
-            yAxis: {
-                orientation: "opposite",
-                formatString: "a",
-            },
-            defaultSeries: {
-                defaultPoint_marker: {
-                    type: "circle",
-                    size: 8,
-                    fill: "white",
-                    outline: { width: 2, color: "currentColor" },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
                 },
             },
-            title_label_text: "API Propia y Cancer Deaths",
-            series: [
-                {
-                    name: "Hombres",
-                    points: ar_ym,
-                },
-                {
-                    name: "Mujeres",
-                    points: ar_yw,
-                },
-                {
-                    name: "Media",
-                    points: ar_ty,
-                },
-                {
-                    name: "Edad 0-50",
-                    points: azfSOS,
-                },
-                {
-                    name: "Edad 50-70",
-                    points: afsSOS,
-                },
-                {
-                    name: "Edad 70",
-                    points: asSOS,
-                },
-            ],
         });
     }
-
-    onMount(getData);
 </script>
+
+<main>
+    <br />
+    <br />
+    <Button id="back" outline color="secondary" on:click={pop}>Atrás</Button>
+    <br />
+    <br />
+    <br />
+    <br />
+    <h4>
+        Gráfico de barras sobre CO2 por pais y año de Jesus Vena Grupo 22
+        mezclado con la mia.
+    </h4>
+    <canvas id="myChart" width="10" height="3" />
+</main>
 
 <svelte:head>
     <script
-        src="https://code.jscharting.com/latest/jscharting.js"
+        src="https://cdn.jsdelivr.net/npm/chart.js"
         on:load={loadGraph}></script>
 </svelte:head>
-<main>
-    <div
-        id="chartDiv"
-        style="max-width: 740px;height: 400px;margin: 0px auto;"
-    />
-</main>
